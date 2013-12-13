02dacd4238659d16cbc27ac88df003f96efe97cc
Clashsoft <clash2@hotmail.de>
2013-12-13 14:40:55 +0100

    Added proxy, added event handler

--- clashsoft/mods/avi/AdvancedVillagerInteraction.java
+++ clashsoft/mods/avi/AdvancedVillagerInteraction.java
@@ -1,18 +1,32 @@
 package clashsoft.mods.avi;
 
-import net.minecraftforge.common.Configuration;
-
+import clashsoft.mods.avi.common.AVICommonProxy;
+import clashsoft.mods.avi.common.AVIEventHandler;
 import clashsoft.mods.avi.common.AVIPacketHandler;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkRegistry;
+
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.common.MinecraftForge;
 
 @Mod(modid = "AdvancedVillagerInteraction", name = "Advanced Villager Interaction", version = AdvancedVillagerInteraction.VERSION)
 @NetworkMod(channels = { "AVI" }, clientSideRequired = true, serverSideRequired = false, packetHandler = AVIPacketHandler.class)
 public class AdvancedVillagerInteraction
 {
 	public static final String	VERSION	= "1.6.4-0";
+	
+	@Instance("AdvancedVillagerInteraction")
+	public static AdvancedVillagerInteraction instance;
+	
+	@SidedProxy(clientSide = "clashsoft.mods.avi.client.AVIClientProxy", serverSide = "clashsoft.mods.avi.common.AVICommonProxy")
+	public static AVICommonProxy proxy;
+	
+	public static AVIEventHandler eventHandler = new AVIEventHandler();
 	
 	public void preLoad(FMLPreInitializationEvent event)
 	{
@@ -25,6 +39,7 @@ public class AdvancedVillagerInteraction
 	
 	public void load(FMLInitializationEvent event)
 	{
-		
+		NetworkRegistry.instance().registerGuiHandler(this, proxy);
+		MinecraftForge.EVENT_BUS.register(eventHandler);
 	}
 }
---
+++ clashsoft/mods/avi/common/AVIEventHandler.java
@@ -0,0 +1,11 @@
+package clashsoft.mods.avi.common;
+
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+
+public class AVIEventHandler
+{
+	public void onEntityJoined(EntityJoinWorldEvent event)
+	{
+		
+	}
+}
