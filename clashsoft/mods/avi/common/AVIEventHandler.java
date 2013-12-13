bae4c0a94269fea4f04404a1cdc3acfb0cc35bed
Clashsoft <clash2@hotmail.de>
2013-12-13 14:48:02 +0100

    Added advanced villager class, ordinary villagers are replaced on spawn

--- clashsoft/mods/avi/common/AVIEventHandler.java
+++ clashsoft/mods/avi/common/AVIEventHandler.java
@@ -1,11 +1,30 @@
 package clashsoft.mods.avi.common;
 
+import clashsoft.mods.avi.entity.EntityAdvancedVillager;
+
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 public class AVIEventHandler
 {
 	public void onEntityJoined(EntityJoinWorldEvent event)
 	{
-		
+		if (!event.world.isRemote)
+		{
+			if (event.entity instanceof EntityVillager)
+			{
+				EntityVillager villager = (EntityVillager) event.entity;
+				
+				NBTTagCompound copy = new NBTTagCompound();
+				event.entity.writeToNBT(copy);
+				
+				EntityAdvancedVillager villager2 = new EntityAdvancedVillager(event.world, villager.getProfession());
+				villager2.readFromNBT(copy);
+				
+				villager.setDead();
+				event.world.spawnEntityInWorld(villager2);
+			}
+		}
 	}
 }
---
+++ clashsoft/mods/avi/entity/EntityAdvancedVillager.java
@@ -0,0 +1,18 @@
+package clashsoft.mods.avi.entity;
+
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.world.World;
+
+public class EntityAdvancedVillager extends EntityVillager
+{
+
+	public EntityAdvancedVillager(World world)
+	{
+		super(world);
+	}
+	
+	public EntityAdvancedVillager(World world, int profession)
+	{
+		super(world, profession);
+	}
+}
