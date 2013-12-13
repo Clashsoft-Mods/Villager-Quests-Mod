37fa61a2657bb9d8e43765fd3978fcaba1add6b6
Clashsoft <clash2@hotmail.de>
2013-12-13 17:07:08 +0100

    Minor changes

--- clashsoft/mods/avi/common/AVIEventHandler.java
+++ clashsoft/mods/avi/common/AVIEventHandler.java
@@ -12,20 +12,17 @@ public class AVIEventHandler
 	@ForgeSubscribe
 	public void onEntityJoined(EntityJoinWorldEvent event)
 	{
-		if (!event.world.isRemote)
+		if (event.entity.getClass() == EntityVillager.class)
 		{
-			if (event.entity.getClass() == EntityVillager.class)
-			{
-				EntityVillager villager = (EntityVillager) event.entity;
-				
-				NBTTagCompound copy = new NBTTagCompound();
-				villager.writeToNBT(copy);
-				villager.setDead();
-				
-				EntityAdvancedVillager villager2 = new EntityAdvancedVillager(event.world, villager.getProfession());
-				villager2.readFromNBT(copy);
-				event.world.spawnEntityInWorld(villager2);
-			}
+			EntityVillager villager = (EntityVillager) event.entity;
+			
+			NBTTagCompound copy = new NBTTagCompound();
+			villager.writeToNBT(copy);
+			villager.setDead();
+			
+			EntityAdvancedVillager villager2 = new EntityAdvancedVillager(event.world, villager.getProfession());
+			villager2.readFromNBT(copy);
+			event.world.spawnEntityInWorld(villager2);
 		}
 	}
 }
