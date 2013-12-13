02be3116384b15e7956917e8282f4e96f584fdf9
Clashsoft <clash2@hotmail.de>
2013-12-13 15:13:45 +0100

    Added quest class

---
+++ clashsoft/mods/avi/quest/Quest.java
@@ -0,0 +1,15 @@
+package clashsoft.mods.avi.quest;
+
+public class Quest
+{
+	public String name;
+	public String description;
+	public int reward;
+	
+	public Quest(String name, String description, int reward)
+	{
+		this.name = name;
+		this.description = description;
+		this.reward = reward;
+	}
+}
